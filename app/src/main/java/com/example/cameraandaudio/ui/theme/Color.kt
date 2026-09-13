d7e1a1e510ed8d509cccb7510b51fd1a17b52211
chinkychinky003-afk <chinkychinky003@gmail.com>
2026-09-13 17:00:37 +0530

    Add color definitions in Color.kt

---
+++ app/src/main/java/com/example/cameraandaudio/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.cameraandaudio.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AccentAmber = Color(0xFFE8C07D)
+val SurfaceDark = Color(0xFF1C1A17)
+val SurfaceCard = Color(0xFF242220)
